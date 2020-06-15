b18a5d8ded1d954fed0c99a278685b71d6cb1491
agentnova <arjunaj2234@gmail.com>
2020-06-15 13:33:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 from pyrogram import Client,Filters
 
-
-
 app = Client(
-    "mynewfindtruebothj",
-    api_id= "988003",
-    api_hash="a5a734ad1a84a7e62c21f7c5898831d7",
-    bot_token="1115604796:AAFZmGuBEWdeHb66OUOB3zBrT-pSgpTCKYc"
+    "mybot",
+    api_id= "",
+    api_hash="",
+    bot_token=""
 )
 
 @app.on_message(Filters.command(["start"]))
@@ -15,17 +13,16 @@ def start(client, message):
         chat_id=message.chat.id,
         text=f"Hi {message.from_user.first_name}"
       )
-    client.send_message(
-           chat_id=message.chat.id,
-           text="Enter the number to search"
-)
 @app.on_message(Filters.text)
 def echo(client, message):
-    pro = client.send_message(
+    client.send_message(
        chat_id = message.chat.id,
-       text = "Searching...",
-       reply_to_message_id=message.message_id
+       text = "where are you from"
         )
-    num=message.text
+    place=message.text
+    client.send_message(
+        chat_id = message.chat.id, 
+        text = "you are from"+place
+)
 
 app.run()
