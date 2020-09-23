f5e6b814cf07d349f23710147a75e2e8842414c8
agentnova <arjunaj2234@gmail.com>
2020-09-23 13:47:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,20 +9,11 @@ app = Client(
 
 @app.on_message(Filters.command(["start"]))
 def start(client, message):
-    client.send_message(
-        chat_id=message.chat.id,
-        text=f"Hi {message.from_user.first_name}"
-      )
+    client.send_message(chat_id=message.chat.id,text=f"Hi {message.from_user.first_name}")
+    client.send_message(chat_id = message.chat.id, text = "where are you from")
 @app.on_message(Filters.text)
 def echo(client, message):
-    client.send_message(
-       chat_id = message.chat.id,
-       text = "where are you from"
-        )
     place=message.text
-    client.send_message(
-        chat_id = message.chat.id, 
-        text = "you are from"+place
-)
+    client.send_message(chat_id = message.chat.id, text = "you are from"+place)
 
 app.run()
