41cfb05f6f80dbe1c283e496321e5dfd45d4c4f3
agentnova <arjunaj2234@gmail.com>
2020-11-29 18:49:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
-from pyrogram import Client,Filters
+from pyrogram import Client,filters
 
 app = Client(
-    "mybot",
+    "mybotname",
     api_id= "",
     api_hash="",
     bot_token=""
 )
 
-@app.on_message(Filters.command(["start"]))
+@app.on_message(filters.command(["start"]))
 def start(client, message):
     client.send_message(chat_id=message.chat.id,text=f"Hi {message.from_user.first_name}")
     client.send_message(chat_id = message.chat.id, text = "where are you from")
-@app.on_message(Filters.text)
+
+@app.on_message(filters.text)
 def echo(client, message):
     place=message.text
     client.send_message(chat_id = message.chat.id, text = "you are from"+place)
